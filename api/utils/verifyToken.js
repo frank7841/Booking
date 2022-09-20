8afa510b34ee07811db0d136d5f78f307e9dbb35
frank7841 <franklinewax@gmail.co>
2022-09-20 16:11:09 +0300

    update on next call

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -14,7 +14,7 @@ export const verifyToken = (req,res,next)=>{
     })
 }
 export const verifyUser =(req,res,next)=>{
-    verifyToken(req,res, ()=>{
+    verifyToken(req,res,next, ()=>{
         if(req.user.id== req.params.id|| req.user.isAdmdin) {
             next()
         }
@@ -25,7 +25,7 @@ export const verifyUser =(req,res,next)=>{
     })
 }
 export const verifyAdmin =(req,res,next)=>{
-    verifyToken(req,res, ()=>{
+    verifyToken(req,res,next, ()=>{
         if(req.user.isAdmdin) {
             next()
         }
